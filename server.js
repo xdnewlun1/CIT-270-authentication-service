68b7b3e5a8fb6394b88008854d713b7b88dc082f
xdnewlun1 <xdnewlun1@users.noreply.github.com>
2022-05-13 13:47:15 -0600

    added redis support, and hashed password

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express'); //Express Import
+const redis = require('redis');
+const client = redis.createClient();
 const bodyParser = require('body-parser');
+const md5 = require('md5');
 const { response } = require('express');
 const PORT = 3000;
 const app = express(); //Initialize the Library for Use
+
+client.on('error', (err) => console.log("Redis Client Error", err));
+
+client.connect();
+client.on('connect', function(){
+    console.log('Conected!');
+});
 
 app.use(bodyParser.json());
 
 app.listen(PORT,()=>{console.log("Listening on Port: " + PORT)});
 
-app.post('/login',(request,response)=>{//POSt is new information to the server
+app.post('/login',async (request,response)=>{//POSt is new information to the server
     console.log("Request Reieved");
     const loginRequest = request.body;
-    if (loginRequest.userName == "xandernew" && loginRequest.password == "P@SSWORD1"){
+    var md5_pass = await client.hGet('password', loginRequest.userName);
+    console.log(loginRequest.userName);
+    console.log(md5_pass);
+    //Search DB for username and get current Hash
+    loginRequest.password = md5(loginRequest.password)
+    if (loginRequest.password == md5_pass){
         response.status(200);
         response.send("Welcome")
     }else{
